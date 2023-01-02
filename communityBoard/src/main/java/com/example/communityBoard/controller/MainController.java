08fd333debe6c19cf374a9d8c34c6a9db9da83c4
Limworld98 <phoenix1228@daum.net>
2023-01-02 23:04:29 +0900

    add basic Page and Controller

---
+++ communityBoard/src/main/java/com/example/communityBoard/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.example.communityBoard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("mainPage")
+    public String hello() {
+        return "index";
+    }
+}
